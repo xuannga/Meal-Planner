00a4d9d50011810e8f9ec8ce315b3677001d4d5a
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-08-16 19:14:04 -0700

    Added Api ingredients route, cleaned us cupboard delete by removing body info - not needed . Updated index.js in controllers/api folder to include the ingredients path/route. Updated storyboard.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const cupboard = require('./cupboard');
+const ingredients = require('./ingredients');
+
 
 router.use('/cupboard', userRoutes);
+router.use('/ingredients', ingredients);
 
 
 module.exports = router;
---
+++ controllers/api/ingredients.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { Cupboard, Ingredient } = require('../../models');
+const withAuth = require('../../utils/auth.js');
+router.post('/',   async (req, res) => {
+    try {
+      const newIngredient = await Ingredient.create({
+          ...req.body
+  
+      });
+      
+      return res.json(newIngredient)
+   
+      // });
+    } catch (err) {
+      console.log(err)
+     
+      res.status(500).json(err);
+    }
+  });
+  
+  //Update Ingredient item - may only need to update quantity
+  router.put('/:id',   async (req, res) => {
+    try {
+      const updateIngredient = await Ingredient.update(
+        {
+          ...req.body
+        },
+        {where: {
+          id: req.params.id}
+    });
+      
+      return res.json(updateBlog)
+   
+      // });
+    } catch (err) {
+      console.log(err)
+     
+      res.status(500).json(err);
+    }
+  });
+  
+  router.delete('/:id',   async (req, res) => {
+    try {
+      const oneIngredient = await Ingredient.destroy(
+        {where: {
+          id: req.params.id}
+    });
+      
+      return res.json(oneIngredient)
+   
+      // });
+    } catch (err) {
+      console.log(err)
+     
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/',   async (req, res) => {
+    try {
+      const allIngredient = await Ingredient.findAll();
+      
+      return res.json(allIngredient)
+   
+      // });
+    } catch (err) {
+      console.log(err)
+     
+      res.status(500).json(err);
+    }
+  });
+
+
+  module.exports=router;
