c66c70f85aa3063d1b5de5351f605808f401463b
Kayla Morris <moreiskay14@gmail.com>
2021-08-21 12:53:22 -0700

    merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,30 +7,15 @@ router.get('/login', (req, res) => {
 
 router.get('/dashboard', withAuth, (req, res) => {
     res.render('dashboard');
-})
+});
 
 router.get('/', (req, res) => {
     res.render('homepage')
-})
+});
 
 router.get('/planning', withAuth, (req, res) => {
     res.render('planningMeals')
-})
+});
 
-router.get('/meals', (req, res) => {
-    try {
-        
-        // get meals from database
-
-        // serialize the data
-        
-        // pass the data into render res.render('meals', data)
-        res.render('meals')
-
-    } catch (err) {
-        console.error(err)
-        res.status(500).json(err)
-    }
-})
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const cupboard = require('./cupboard');
+const meals = require('./meals');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/cupboard', cupboard);
+router.use('/meals', meals);
 
 module.exports = router;
