09e1d63430a607e90b48df093107e8b5218c447f
j92pruitt <j92pruitt@gmail.com>
2021-08-22 17:27:19 -0700

    Added single meal view and form for adding an ingredient to the view.

--- controllers/api/ingredients.js
+++ controllers/api/ingredients.js
@@ -3,14 +3,23 @@ const { Cupboard, Ingredient } = require('../../models');
 const withAuth = require('../../utils/auth.js');
 router.post('/',   async (req, res) => {
     try {
-      const newIngredient = await Ingredient.create({
-          ...req.body
-  
-      });
+      const cupboardData = await Cupboard.findOne({
+        where: {
+          name: req.body.cupboardItem,
+          user_id: req.user.id
+        }
+      })
+
+      cupboard_id = cupboardData.id;
+
+      const dbResponse = await Ingredient.create({
+        cupboard_id,
+        meal_id: req.body.meal_id,
+        quantity: req.body.quantity,
+      })
       
-      return res.json(newIngredient)
+      return res.json(dbResponse)
    
-      // });
     } catch (err) {
       console.log(err)
      
