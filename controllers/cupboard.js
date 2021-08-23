afbc0fb36d14cef782c7e0adb0763e1e8831e6e0
Kayla Morris <moreiskay14@gmail.com>
2021-08-23 14:33:14 -0700

    fixed some errors in cupboard.js

--- controllers/cupboard.js
+++ controllers/cupboard.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { Cupboard } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
         const Refrigitems = await Cupboard.findAll(
             { where: 
