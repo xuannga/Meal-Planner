4a27e889925cbf8ac4c8985c3f52aa5e410c7af2
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-09-13 19:38:51 -0700

    added compression

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
-
+app.use(compression({ filter: shouldCompress }));
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
